559b380640cd8d6076c6c21476c87905fb67dbbb
João Carvalho <joaomcarvalho@sapo.pt>
2019-05-25 19:27:36 +0100

    merge all changes

--- projecto_pl1.m
+++ projecto_pl1.m
@@ -9,6 +9,7 @@ all_labels = importfile('HAPT Data Set/RawData/labels.txt', '%f%f%f%f%f%[^\n\r]'
 close all
 saveSteps = zeros(200,1);
 contador=1;
+
 for acc_file = {{'01','01'}, {'02','01'}, {'03','02'}, {'04','02'}, {'05','03'}, {'06','03'}, {'07','04'}, {'08','04'}, {'09','05'}, {'10','05'}}
 
     %% ex 1, 2 e 3
@@ -32,6 +33,7 @@ for acc_file = {{'01','01'}, {'02','01'}, {'03','02'}, {'04','02'}, {'05','03'},
     activities={'W','W\_U','W\_D','SIT','STAND',...
     'LAY','STAND\_SIT','SIT\_STAND','SIT\_LIE','LIE\_SIT',...
     'STAD\_LIE','LIE\_STAND'};
+    colours={'y','m','c','r','g','b','w','k','k','k','k','k'};
     Sensors={'ACC\_X','ACC\_Y','ACC\_Z'};
     
     % data size
@@ -125,7 +127,7 @@ for acc_file = {{'01','01'}, {'02','01'}, {'03','02'}, {'04','02'}, {'05','03'},
 % 4.2
 %segunda implementacao aboradando agora apenas o eixo dos z's nao esta a
 %funcionar
-
+%{
 
 numeroElementos1=0;
 total1=0;
@@ -144,9 +146,9 @@ for k=1:numel(ix_labels)
         else
             f = -Fs/2+Fs/(2*N):Fs/N:Fs/2-Fs/(2*N);
         end
-        close all
-        plot(f,xdft);
-        hold on
+        %close all
+        %plot(f,xdft);
+        %hold on
         [pks,locs] = findpeaks(abs(xdft),'MinPeakProminence',8);
             index=find(f(locs)>-0.00001 & f(locs)<0.00001);
         if index > 1
@@ -163,8 +165,8 @@ end
 
 %faltam em ambos os casos o desvio padrao
 %}
-%% 4.3
-
+%% 4.3 - Com DFT
+%{
 peaks_X = zeros(numel(ix_labels),1);
 peaks_Y = zeros(numel(ix_labels),1);
 peaks_Z = zeros(numel(ix_labels),1);
@@ -197,7 +199,7 @@ for k=1:numel(ix_labels)
     %hold on
     if all_labels(ix_labels(k),3) > 3
         [pks,locs] = findpeaks(xdft,'MinPeakHeight', 1); % static
-        [pks,locs_y] = findpeaks(ydft,'MinPeakHeight', 1); % static
+        [pks_y,locs_y] = findpeaks(ydft,'MinPeakHeight', 1); % static
         [pks_z,locs_z] = findpeaks(zdft,'MinPeakHeight', 1); % static
     else
         [pks,locs] = findpeaks(xdft,'MinPeakProminence', 8);
@@ -206,22 +208,32 @@ for k=1:numel(ix_labels)
     end
     
     index = find(f(locs)>-0.00001 & f(locs)<0.00001);
+    
     f1 = f(locs);
     f2 = f(locs_y);
     f3 = f(locs_z);
     
-    if index > 0 & index < numel(f1) & index < numel(f2) & index < numel(f3)
+    if index ~= 0 & index < numel(f1)
         peaks_X(k) = f1(index+1);
-        peaks_Y(k) = f2(index+1);
-        peaks_Z(k) = f3(index+1);
-    %elseif index > 0
-        %peaks_X(k) = f1(index);
     else
-        disp(' no value ')
         peaks_X(k) = 0;
+    end
+    
+    
+    if indexY ~= 0 & index < numel(f2)
+        peaks_Y(k) = f2(index+1);
+    else
         peaks_Y(k) = 0;
+    end
+    
+    
+    if index ~= 0 & index < numel(f3)
+       peaks_Z(k) = f3(index+1);
+    else
         peaks_Z(k) = 0;
     end
+    
+    
     
     %plot(f1(index+1),10,'or');
     %pause();
@@ -230,145 +242,189 @@ end
 %hold on
 %scatter3(peaks_X,peaks_Y,peaks_Z, 'r', 'filled');
 
-
+figure(41)
+title('Actividades estáticas vs. dinâmicas - DFT');
 XDin=peaks_X(13:numel(peaks_X));
 YDin=peaks_Y(13:numel(peaks_Y));
 ZDin=peaks_Z(13:numel(peaks_Z));
 XStat=peaks_X(1:12);
 YStat=peaks_Y(1:12);
 ZStat=peaks_Z(1:12);
-
 hold on
 scatter3(XDin,YDin,ZDin, 'r', 'filled')
 scatter3(XStat,YStat,ZStat, 'b', 'filled')
+grid on
 
-%}
+figure(42)
+title('Actividades dinâmicas - DFT');
+hold on
+for k=13:numel(ix_labels)
+    if peaks_X(k:k) > 0
+        ix = find(strcmp(activities, activities{all_labels(ix_labels(k),3)}));
+        colour = char(colours(ix));
+        scatter3(peaks_X(k:k),peaks_Y(k:k),peaks_Z(k:k), colour, 'filled')
+    end
+end
+legend({'W','W\_U','W\_D'},'Location','southwest')
+hold off
+grid on
 
-%{
-figure(4)
-picsX = zeros(numel(ix_labels),1);
-picsY= zeros(numel(ix_labels),1);
-picsZ= zeros(numel(ix_labels),1);
+%% 4.3 - sem DFT
+
+peaks_X = zeros(numel(ix_labels),1);
+peaks_Y = zeros(numel(ix_labels),1);
+peaks_Z = zeros(numel(ix_labels),1);
 for k=1:numel(ix_labels)
     %vai carregar a informacao dos 3 eixos
     x=data(all_labels(ix_labels(k),4): all_labels(ix_labels(k),5),1);
     y=data(all_labels(ix_labels(k),4): all_labels(ix_labels(k),5),2);
     z=data(all_labels(ix_labels(k),4): all_labels(ix_labels(k),5),3);
-   
+    x=abs(x);
+    y=abs(y);
+    z=abs(z);
+    
     %delimita o ponto medio para de seguida determinar os picos
-    
-    %x
-    [f,xdft] = my_fft(x.*hamming(numel(x)),Fs);
-    max_x = max(abs(xdft));
-    min_mag = max_x - (0.2*max_x);
-    [pks,locs] = findpeaks(abs(xdft),'MINPEAKHEIGHT', min_mag);
+    magNoG = x - mean(x);
+    minPeakHeight = std(x);
+    [~, locs] = findpeaks(magNoG, 'MINPEAKHEIGHT', minPeakHeight);
     if numel(locs) > 0
-        picsX(k)= xdft(locs(1));  
+        peaks_X(k)= x(locs(1));  
     end
-    
-    % plot x for debug peaks
-    %figure;
-    %plot(abs(xdft))
-    %hold on
-    %plot(locs,pks,'ro')
-
     
     %y
-    [f,ydft] = my_fft(y.*hamming(numel(y)),Fs);
-    max_x = max(abs(ydft));
-    min_mag = max_x-(0.2*max_x);
-    [pks,locs] = findpeaks(abs(ydft),'MINPEAKHEIGHT', min_mag);
+    magNoG = y - mean(y);
+    minPeakHeight = std(y);
+    [~, locs] = findpeaks(magNoG, 'MINPEAKHEIGHT', minPeakHeight);
     if numel(locs) > 0
-        picsY(k)= ydft(locs(1));  
+        peaks_Y(k)= y(locs(1));  
     end
-    
-    % plot y for debug peaks
-    %plot(abs(ydft))
-    %plot(locs,pks,'ro')
-
     %z
-    [f,zdft] = my_fft(z.*hamming(numel(z)),Fs);
-    max_x = max(abs(zdft));
-    min_mag = max_x-(0.2*max_x);
-    [pks,locs] = findpeaks(abs(zdft),'MINPEAKHEIGHT', min_mag);
+    magNoG = z - mean(z);
+    minPeakHeight = std(z);
+    [pks, locs] = findpeaks(magNoG, 'MINPEAKHEIGHT', minPeakHeight);
     if numel(locs) > 0
-        picsZ(k)= zdft(locs(1));  
+        peaks_Z(k)= z(locs(1));  
     end
-
-    % plot z for debug peaks
-    %plot(abs(zdft))
-    %plot(locs,pks,'ro')
-    
-    hold off
 end
 
-XDin=picsX(13:numel(picsX));
-YDin=picsY(13:numel(picsX));
-ZDin=picsZ(13:numel(picsX));
-XStat=picsX(1:12);
-YStat=picsY(1:12);
-ZStat=picsZ(1:12);
-
+figure(5)
+title('Actividades estáticas vs. dinâmicas - raw');
+XDin=peaks_X(13:numel(peaks_X));
+YDin=peaks_Y(13:numel(peaks_Y));
+ZDin=peaks_Z(13:numel(peaks_Z));
+XStat=peaks_X(1:12);
+YStat=peaks_Y(1:12);
+ZStat=peaks_Z(1:12);
 hold on
 scatter3(XDin,YDin,ZDin, 'r', 'filled')
 scatter3(XStat,YStat,ZStat, 'b', 'filled')
+legend({'Dinâmicas', 'Estáticas'},'Location','southwest')
+hold off
+grid on
 
-
-
-    %% ex. 5.
-    % Freq/Time min |Power
-    % STFT no eixo Z para um ficheiro de dados ?? escolha
-    
-    i = 3; %eixo z
-    % j = 13; %activity
-    for j=1:numel(ix_labels)
-        activity = data(all_labels(ix_labels(j),4): all_labels(ix_labels(j),5),i);
-
-        N = numel(activity);
-        Tframe= 0.128; %largura da janela em analise em s
-        Toverlap = 0.064; % sobreposi????o das janelas em s
-        Nframe= round(Tframe*Fs); %numero de amostras na janela
-        Noverlap = round(Toverlap*Fs); % numero de amostras sobrepostas
-
-        h = hamming(Nframe); % janela de hamming
-
-        if mod(Nframe, 2)==0
-            f_frame = -Fs/2:Fs/Nframe:Fs/2-Fs/Nframe;
-        else
-            f_frame = -Fs/2+Fs/(2*Nframe):Fs/Nframe:Fs/2-Fs/(2*Nframe);
-        end
-
-        freq_relev = [];
-        nframes = 0; %para guardar freq relevantes
-        tframes = [];
-
-        % itera sobre sinal da actividade com janelas sobrepostas
-        % ver na fp 9...
-        for ii = 1:Nframe-Noverlap:N-Nframe
-            % aplicar a janela ao sinal do tempo
-            x_frame = activity(ii:ii+Nframe-1).*h;
-
-            % obter a magnitude da fft do sinal
-            m_X_frame=abs(fftshift(fft(x_frame)));
-
-            % obter o maximo da magnitude do sinal
-            m_X_frame_max = max(m_X_frame);
-
-            % encontrar os indices do maximo da magnitude do sinal
-            ind = find(abs(m_X_frame-m_X_frame_max)<0.001);
-
-            % encontrar as frequencias correspondentes ao maximo de 
-            %freq_relev = [freq_relev, f_frame(ind(2))]; % buscar o indice 2 para a frequencia positiva
-
-            nframes = nframes+1;
-        end
+% geracao do plot nao funciona:
+figure(6)
+title('Actividades dinâmicas - raw data');
+tmp=zeros(50,3);
+hold on
+for k=13:numel(ix_labels)
+    if peaks_X(k:k) > 0
+        ix = find(strcmp(activities, activities{all_labels(ix_labels(k),3)}));
+        colour = char(colours(ix));
+        %scatter3(peaks_X(k:k),peaks_Y(k:k),peaks_Z(k:k), colour, 'filled')
+        tmp(k,1)=peaks_X(k:k);
+        tmp(k,2)=peaks_Y(k:k);
+        tmp(k,3)=peaks_Z(k:k);
     end
+end
+scatter3(nonzeros(tmp(:,1)),nonzeros(tmp(:,2)),nonzeros(tmp(:,3)), colour, 'filled')
+legend({'W','W\_U','W\_D'},'Location','southwest')
+hold off
+grid on
+%}
+
+
+%% ex. 5.
+% Freq/Time min |Power
+% STFT no eixo Z para um ficheiro de dados ?? escolha
+%{
+
+% j = 13; %activity
+for j=1:numel(ix_labels)
+    activity = data(all_labels(ix_labels(j),4): all_labels(ix_labels(j),5),3);
+    %{
+    N = numel(activity);
+    Tframe= 0.128; %largura da janela em analise em s
+    Toverlap = 0.064; % sobreposicao das janelas em s
+    Nframe= round(Tframe*Fs); %numero de amostras na janela
+    Noverlap = round(Toverlap*Fs); % numero de amostras sobrepostas
+
+    h = hamming(Nframe); % janela de hamming
+
+    if mod(Nframe, 2)==0
+        f_frame = -Fs/2:Fs/Nframe:Fs/2-Fs/Nframe;
+    else
+        f_frame = -Fs/2+Fs/(2*Nframe):Fs/Nframe:Fs/2-Fs/(2*Nframe);
+    end
+
+    freq_relev = [];
+    nframes = 0; %para guardar freq relevantes
+    tframes = [];
+
+    % itera sobre sinal da actividade com janelas sobrepostas
+    % ver na fp 9...
+    for ii = 1:Nframe-Noverlap:N-Nframe
+        % aplicar a janela ao sinal do tempo
+        x_frame = activity(ii:ii+Nframe-1).*h;
+
+        % obter a magnitude da fft do sinal
+        m_X_frame=abs(fftshift(fft(x_frame)));
+
+        % obter o maximo da magnitude do sinal
+        m_X_frame_max = max(m_X_frame);
+
+        % encontrar os indices do maximo da magnitude do sinal
+        ind = find(abs(m_X_frame-m_X_frame_max)<0.001);
+
+        % encontrar as frequencias correspondentes ao maximo de 
+        %freq_relev = [freq_relev, f_frame(ind(2))]; % buscar o indice 2 para a frequencia positiva
+
+        nframes = nframes+1;
+
+
+    end
+    %}
+
+
+
+    wlen = 0.128; %largura da janela em analise em s
+    hop = 0.064; % sobreposicao das janelas em s
+    nfft = round(wlen*Fs); %numero de amostras na janela
+
+    % stft matrix size estimation and preallocation
+    %NUP = ceil((1+nfft)/2);     % calculate the number of unique fft points
+    %L = 1+fix((xlen-wlen)/hop); % calculate the number of signal frames
+    %STFT = zeros(NUP, L);       % preallocate the stft matrix
+    %win = blackman(wlen, 'periodic'); 
+
+    figure(1)
+    stft(activity,'Window',kaiser(256,5),'OverlapLength',Fs);
+    colormap bone
+    view(-45,65)
+
+
+
+   % plot(activity)
+    %xlabel('t [s]')
+    %ylabel('f [Hz]')
+    %title('Sequencia de frequencias por janelas');
+end
 %}
 
 end
+
 media= mean(nonzeros(saveSteps))
 desvioPadrao=std(nonzeros(saveSteps))
 
 hold off
-grid on
 view(-80, 22)
